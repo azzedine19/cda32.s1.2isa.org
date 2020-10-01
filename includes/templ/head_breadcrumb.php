dbdecde05475b1171208eae306275dc93a5c69df
azzedine19 <azzedineboukhanoufa@yahoo.fr>
2020-10-01 17:53:37 +0200

    background

--- includes/templ/head_breadcrumb.php
+++ includes/templ/head_breadcrumb.php
@@ -1,4 +1,4 @@
-<section class="hero-wrap hero-wrap-2 js-fullheight" size="10px" style="background-image: url('images/harley.jpg');" data-stellar-background-ratio="0.5">
+<section class="hero-wrap hero-wrap-2 js-fullheight" size="10px" style="background-image: url('images/headermoto.jpg');" data-stellar-background-ratio="0.5">
     <div class="overlay"></div>
     <div class="container">
         <div class="row no-gutters slider-text js-fullheight align-items-end justify-content-start">
